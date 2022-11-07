eaa2619b50122d929719aef6a62023c36e931763
rodsonnazario <rodson.nazario@gmail.com>
2022-11-06 21:30:21 -0300

    tratamento de exceção na connectionfactory

--- src/br/com/alura/jdbc/factory/ConnectionFactory.java
+++ src/br/com/alura/jdbc/factory/ConnectionFactory.java
@@ -1,4 +1,5 @@
 package br.com.alura.jdbc.factory;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -14,13 +15,17 @@ public class ConnectionFactory {
 		ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
 		comboPooledDataSource.setJdbcUrl("jdbc:mariadb://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC");
 		comboPooledDataSource.setUser("root");
-		comboPooledDataSource.setPassword("root");		
+		comboPooledDataSource.setPassword("root");
 		comboPooledDataSource.setMaxPoolSize(15);
 
 		this.dataSource = comboPooledDataSource;
 	}
 
-	public Connection recuperarConexao() throws SQLException {
-		return this.dataSource.getConnection();
+	public Connection recuperarConexao() {
+		try {
+			return this.dataSource.getConnection();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
