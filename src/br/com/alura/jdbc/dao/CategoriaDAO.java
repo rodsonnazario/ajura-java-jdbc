31b343dd8e2480f04ab4dca26e76b8c31de7a964
rodsonnazario <rodson.nazario@gmail.com>
2022-11-06 21:34:24 -0300

    ajuste de exceções nos DAOs e finalizado CRUD na ProdutoDAO

--- src/br/com/alura/jdbc/dao/CategoriaDAO.java
+++ src/br/com/alura/jdbc/dao/CategoriaDAO.java
@@ -11,63 +11,68 @@ import br.com.alura.jdbc.model.Categoria;
 import br.com.alura.jdbc.model.Produto;
 
 public class CategoriaDAO {
-	
+
 	private Connection connection;
 
 	public CategoriaDAO(Connection connection) {
 		this.connection = connection;
 	}
-	
-	public List<Categoria> listar() throws SQLException {
 
-		List<Categoria> categorias = new ArrayList<>();
-		String sql = "SELECT id, nome FROM categoria";
+	public List<Categoria> listar() {
 
-		try (PreparedStatement pstm = connection.prepareStatement(sql)) {
-			pstm.execute();
+		try {
+			List<Categoria> categorias = new ArrayList<>();
+			String sql = "SELECT id, nome FROM categoria";
 
-			try(ResultSet rs = pstm.getResultSet()) {
-				
-				while (rs.next()) {
-					Integer id = rs.getInt("id");
-					String nome = rs.getString("nome");					
-					Categoria categoria = new Categoria(id, nome);					
-					categorias.add(categoria);
+			try (PreparedStatement pstm = connection.prepareStatement(sql)) {
+				pstm.execute();
+
+				try (ResultSet rs = pstm.getResultSet()) {
+
+					while (rs.next()) {
+						Integer id = rs.getInt("id");
+						String nome = rs.getString("nome");
+						Categoria categoria = new Categoria(id, nome);
+						categorias.add(categoria);
+					}
 				}
 			}
+			return categorias;
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
 		}
-
-		return categorias;
 	}
 
-	public List<Categoria> listarComProdutos() throws SQLException {
-		Categoria categoria = null;
-		List<Categoria> categorias = new ArrayList<>();
-		String sql = "SELECT c.id, c.nome, p.id, p.nome, p.descricao "
-				+ "FROM categoria c "
-				+ "INNER JOIN produto p ON c.id=p.categoria_id";
+	public List<Categoria> listarComProdutos() {
+		try {
+			Categoria categoria = null;
+			List<Categoria> categorias = new ArrayList<>();
+			String sql = "SELECT c.id, c.nome, p.id, p.nome, p.descricao " + "FROM categoria c "
+					+ "INNER JOIN produto p ON c.id=p.categoria_id";
 
-		try (PreparedStatement pstm = connection.prepareStatement(sql)) {
-			pstm.execute();
+			try (PreparedStatement pstm = connection.prepareStatement(sql)) {
+				pstm.execute();
 
-			try(ResultSet rs = pstm.getResultSet()) {				
-				while (rs.next()) {
-					Integer idCategoria = rs.getInt(1);
-					String nomeCategoria = rs.getString(2);
-					Integer idProduto = rs.getInt(3);
-					String nomeProduto = rs.getString(4);
-					String descricaoProduto = rs.getString(5);
-					
-					if(categoria == null || !categoria.getNome().equals(nomeCategoria)) {
-						categoria = new Categoria(idCategoria, nomeCategoria);						
-						categorias.add(categoria);	
+				try (ResultSet rs = pstm.getResultSet()) {
+					while (rs.next()) {
+						Integer idCategoria = rs.getInt(1);
+						String nomeCategoria = rs.getString(2);
+						Integer idProduto = rs.getInt(3);
+						String nomeProduto = rs.getString(4);
+						String descricaoProduto = rs.getString(5);
+
+						if (categoria == null || !categoria.getNome().equals(nomeCategoria)) {
+							categoria = new Categoria(idCategoria, nomeCategoria);
+							categorias.add(categoria);
+						}
+						Produto produto = new Produto(idProduto, nomeProduto, descricaoProduto);
+						categoria.adicionarProduto(produto);
 					}
-					Produto produto = new Produto(idProduto, nomeProduto, descricaoProduto);
-					categoria.adicionarProduto(produto);
 				}
+				return categorias;
 			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
 		}
-
-		return categorias;
 	}
 }
--- src/br/com/alura/jdbc/dao/ProdutoDAO.java
+++ src/br/com/alura/jdbc/dao/ProdutoDAO.java
@@ -19,67 +19,117 @@ public class ProdutoDAO {
 		this.connection = connection;
 	}
 
-	public void salvar(Produto produto) throws SQLException {
+	public void salvar(Produto produto) {
+		try {
+			String sql = "INSERT INTO produto (nome, descricao) VALUES (?, ?)";
 
-		String sql = "INSERT INTO produto (nome, descricao) VALUES (?, ?)";
+			try (PreparedStatement pstm = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+				pstm.setString(1, produto.getNome());
+				pstm.setString(2, produto.getDescricao());
+				pstm.execute();
 
-		try (PreparedStatement pstm = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
-			pstm.setString(1, produto.getNome());
-			pstm.setString(2, produto.getDescricao());
-			pstm.execute();
-
-			try (ResultSet rs = pstm.getGeneratedKeys()) {
-				while (rs.next()) {
-					produto.setId(rs.getInt(1));
+				try (ResultSet rs = pstm.getGeneratedKeys()) {
+					while (rs.next()) {
+						produto.setId(rs.getInt(1));
+					}
 				}
 			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
 		}
 	}
 
-	public List<Produto> listar() throws SQLException {
+	public void salvarComCategoria(Produto produto) {
+		try {
+			String sql = "INSERT INTO produto (nome, descricao, categoria_id) VALUES (?, ?, ?)";
 
-		List<Produto> produtos = new ArrayList<>();
-		String sql = "SELECT id, nome, descricao FROM produto";
+			try (PreparedStatement pstm = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
 
-		try (PreparedStatement pstm = connection.prepareStatement(sql)) {
-			pstm.execute();
+				pstm.setString(1, produto.getNome());
+				pstm.setString(2, produto.getDescricao());
+				pstm.setInt(3, produto.getCategoriaId());
 
-			try (ResultSet rs = pstm.getResultSet()) {
+				pstm.execute();
 
-				while (rs.next()) {
-					Integer id = rs.getInt("id");
-					String nome = rs.getString("nome");
-					String descricao = rs.getString("descricao");
-					Produto produto = new Produto(id, nome, descricao);
-					produtos.add(produto);
+				try (ResultSet rst = pstm.getGeneratedKeys()) {
+					while (rst.next()) {
+						produto.setId(rst.getInt(1));
+					}
 				}
 			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
 		}
-
-		return produtos;
 	}
 
-	public List<Produto> buscar(Categoria categoria) throws SQLException {
+	public List<Produto> listar() {
+		try {
+			List<Produto> produtos = new ArrayList<>();
+			String sql = "SELECT id, nome, descricao FROM produto";
 
-		List<Produto> produtos = new ArrayList<>();
-		String sql = "SELECT id, nome, descricao FROM produto WHERE categoria_id = ?";
-
-		try (PreparedStatement pstm = connection.prepareStatement(sql)) {
-			pstm.setInt(1, categoria.getId());
-			pstm.execute();
-
-			try (ResultSet rs = pstm.getResultSet()) {
-
-				while (rs.next()) {
-					Integer id = rs.getInt("id");
-					String nome = rs.getString("nome");
-					String descricao = rs.getString("descricao");
-					Produto produto = new Produto(id, nome, descricao);
-					produtos.add(produto);
-				}
+			try (PreparedStatement pstm = connection.prepareStatement(sql)) {
+				pstm.execute();
+				transformarResultSetEmProduto(produtos, pstm);
 			}
-		}
 
-		return produtos;
+			return produtos;
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public List<Produto> buscar(Categoria categoria) {
+		try {
+			List<Produto> produtos = new ArrayList<>();
+			String sql = "SELECT id, nome, descricao FROM produto WHERE categoria_id = ?";
+
+			try (PreparedStatement pstm = connection.prepareStatement(sql)) {
+				pstm.setInt(1, categoria.getId());
+				pstm.execute();
+				transformarResultSetEmProduto(produtos, pstm);
+			}
+
+			return produtos;
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public void deletar(Integer id) {
+		String sql = "DELETE FROM produto WHERE id = ?";
+
+		try (PreparedStatement stm = connection.prepareStatement(sql)) {
+			stm.setInt(1, id);
+			stm.execute();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public void alterar(String nome, String descricao, Integer id) {
+		String sql = "UPDATE produto p SET p.nome = ?, p.descricao = ? WHERE id = ?";
+
+		try (PreparedStatement stm = connection.prepareStatement(sql)) {
+			stm.setString(1, nome);
+			stm.setString(2, descricao);
+			stm.setInt(3, id);
+			stm.execute();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private void transformarResultSetEmProduto(List<Produto> produtos, PreparedStatement pstm) {
+		try (ResultSet rs = pstm.getResultSet()) {
+			while (rs.next()) {
+				Integer id = rs.getInt("id");
+				String nome = rs.getString("nome");
+				String descricao = rs.getString("descricao");
+				Produto produto = new Produto(id, nome, descricao);
+				produtos.add(produto);
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
